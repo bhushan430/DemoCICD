587dcd873709814818f1e6393c31598f8e4753cd
Bhushan Halami <halamibhushan@gmail.com>
2023-10-20 14:35:28 +0530

    first

---
+++ src/main/java/com/amdocs/demoCICD1/DemoCicd1Application.java
@@ -0,0 +1,12 @@
+package com.amdocs.demoCICD1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicd1Application {
+
+	public static void main(String[] args) {
+		System.out.println("h");	}
+
+}
